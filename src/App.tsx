f200cd56a2d796e9701c239c724dc328f451e322
Stephen Chiang <stephen.chiang@webstep.no>
2022-09-14 13:11:33 +0200

    [Lib] - css: Update css rules for font size

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import WaffleChart from './lib/WaffleChart';
 
-const App = () => <WaffleChart />;
+const App = () => (
+    <WaffleChart
+        partA={2}
+        partB={35}
+        partC={86.7}
+        partAlabel={'beer'}
+        partBlabel={'water'}
+        partClabel={'coffee'}
+        partAcolor={'#F7A355'}
+        partBcolor={'#4D93E5'}
+        partCcolor={'#439090'}
+    />
+);
 
 export default App;
